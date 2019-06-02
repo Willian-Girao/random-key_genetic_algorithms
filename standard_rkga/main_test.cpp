df1a5a0d2c12305a2b08ada9911c03647907bafa
Willian-Girao <williansoaresgirao@gmail.com>
2019-06-02 17:30:46 -0300

    getting n-th line file content parsed

--- standard_rkga/main_test.cpp
+++ standard_rkga/main_test.cpp
@@ -1,8 +1,15 @@
 /* Standard libraries */
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cctype>
 #include <array>
 #include <stdlib.h>
 #include <time.h>
+
+
+#include "helper.h"
 
 /* Headers containing classes definitions */
 #include "instance_class.h"
@@ -13,6 +20,35 @@
 using namespace std;
 
 int main() {
+
+  ifstream instance_file("instance_001.txt");
+  string str;
+
+  while(getline(instance_file, str)) {
+    string parsed_line = "";
+    int n_of_values = 0;
+    double *values = new double[30];
+
+    for(char& c : str) {
+      parsed_line += isspace(c) ? ',' : c;
+    }
+
+    while(parsed_line != "") {
+      string number = parsed_line.substr(0, parsed_line.find(","));
+      parsed_line.erase(0, (number.length() + 1));
+
+      (*(values + n_of_values)) = stod(number);
+      n_of_values++;
+    }
+
+    /* Numerical content of the n-th line i the file */
+    for (int i = 0; i < n_of_values; ++i)
+    {
+      cout << (*(values + i)) << endl;
+    }
+    cout << "--------------------------------------------" << endl;
+    
+  }
 
   /*
     3 - number of original nodes
