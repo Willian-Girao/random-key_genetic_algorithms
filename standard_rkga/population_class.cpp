7fbb373e5245fbdbf864bbe9532d6434bfef2b67
Willian-Girao <williansoaresgirao@gmail.com>
2019-08-21 11:37:42 -0300

    inst/chrom description/population

--- standard_rkga/population_class.cpp
+++ standard_rkga/population_class.cpp
@@ -23,7 +23,6 @@ void Population::initializePopulation(int popSize, int chromSize) {
   }
 }
 
-/* Swapts to elements */
 void Population::swap(Chromosome *a, Chromosome *b) {
 	Chromosome temp = *a;
 	*a = *b;
@@ -50,7 +49,7 @@ int Population::partition(Chromosome *array, int low, int high) {
 void Population::quickSort(Chromosome *array, int low, int high) {
 	if (low < high)
 	{
-		// 'pi' is 'partitioning index', array[p] is now at the right position.
+		//'pi' is 'partitioning index', array[p] is now at the right position.
 		int pi = partition(array, low, high);
 
 		quickSort(array, low, (pi - 1));
@@ -63,43 +62,6 @@ void Population::sortByFitness(void) {
   quickSort(population, 0, size - 1);
 }
 
-// Chromosome * Population::getElite(void) {
-//   static Chromosome elite[2];
-
-//   elite[0] = population[0];
-//   elite[1] = population[1];
-
-//   for (int i = 1; i < size; i++) {
-//     if (population[i].getFitness() < elite[0].getFitness()) {
-//       elite[1] = elite[0];
-//       elite[0] = population[i];
-//     } else if (population[i].getFitness() < elite[1].getFitness()) {
-//       elite[1] = population[i];
-//     }
-//   }
-
-//   cout << "Vector 0: " << elite[0].getFitness() << endl;
-//   cout << "Vector 1: " << elite[1].getFitness() << endl;
-
-//   return elite;
-// }
-
-// Chromosome * Population::getRandomPair(void) {
-//   static Chromosome pair[2];
-
-//   int p_a = rand() % 6;
-//   int p_b = rand() % 6;
- 
-//   while (p_a == p_b) {
-//     p_b = rand() % 6;
-//   }
-
-//   pair[0] = population[p_a];
-//   pair[1] = population[p_b];
-
-//   return pair;
-// }
-
 Hallele * Population::matePair(Hallele *a, Hallele *b) {
   double prob;
   Hallele *offspring = new Hallele[population[0].getLength()];
@@ -110,14 +72,10 @@ Hallele * Population::matePair(Hallele *a, Hallele *b) {
   offspring[population[0].getLength()-1].key = 1.0;
   offspring[population[0].getLength()-1].index = 0;
 
-  // cout << "here: " << population[0].getLength() << endl;
-
-  //TODO - it should alter the keys (not their associated index).
   int countHelper = 0;
   for (int i = 1; i < (population[0].getLength() - 1); i++) {
     prob = ((double) rand() / RAND_MAX); /* TODO - this probability should be proportional to the vectors fitness value */
     double present = false;
-  	 //TODO - must add a correction code to correct duplicated indexes.
     if (prob >= 0.5) {
       offspring[i].key = a[i].key;      
       for (int j = 1; j < countHelper+1; ++j)
@@ -146,7 +104,7 @@ Hallele * Population::matePair(Hallele *a, Hallele *b) {
 		{
 			offspring[i].index = b[i].index;
 		} else {
-			//Se what can came here (what number).
+			//See what can came here (what number).
 			int outHelp = 1;
 			for (int x = 1; x < countHelper+1; ++x)
 			{
@@ -186,7 +144,7 @@ Hallele * Population::matePair(Hallele *a, Hallele *b) {
 			{
 				offspring[i].index = a[i].index;
 			} else {
-				//Se what can came here (what number).
+				//See what can came here (what number).
 				int outHelp = 1;
 				for (int x = 1; x < countHelper+1; ++x)
 				{
@@ -203,27 +161,6 @@ Hallele * Population::matePair(Hallele *a, Hallele *b) {
     countHelper++;
   }
 
-  // for (int i = 0; i < population[0].getLength(); ++i)
-  // {
-  // 	// cout << a[i].key << " (" << a[i].index <<  ") ";
-  // 	cout << a[i].index << " ";
-  // }
-  // cout << endl;
-
-  // for (int i = 0; i < population[0].getLength(); ++i)
-  // {
-  // 	// cout << b[i].key << " (" << b[i].index <<  ") ";
-  // 	cout << b[i].index << " ";
-  // }
-  // cout << endl;
-
-  // for (int i = 0; i < population[0].getLength(); ++i)
-  // {
-  // 	// cout << offspring[i].key << " (" << offspring[i].index <<  ") ";
-  // 	cout << offspring[i].index << " ";
-  // }
-  // cout << endl;
-
   return offspring;
 }
 
@@ -234,15 +171,6 @@ void Population::printPopulation(void) {
     cout << endl;
   }
 }
-
-// void Population::printFitness(void) {
-//   for (int i = 0; i < size; i++) {
-//     cout << "Vector " << i << ": " << population[i].getFitness() << endl;
-//   }
-//   cout << endl;
-// }
-
-/* Private methods */
 
 Hallele * Population::getSolutionAsArray(int solIndex) {
 	return population[solIndex].getChromosomeAsArray();
@@ -259,10 +187,8 @@ void Population::introduceMutants(void) {
 	int endIndex = floor((size / 4.0)); //End index when introducing mutants.
 
 	//Reseting 25% of the population (introducing mutants).
-	// cout << "Mutant individuals: " << endIndex << endl;
 	for (int i = 0; i < endIndex; ++i)
 	{
-		// cout << i << endl;
 		population[i].resetChromosome();
 		population[i].setFitness(0.0);
 	}
@@ -276,21 +202,12 @@ void Population::mateIndividuals(void) {
 	int numMutants = floor((size / 4.0));
 	int x = size - ceil((size / 2.0)) - floor((size / 4.0));
 
-	//Indexes of chromosomes from the elit group.
-	// cout << "Mating candidates: " << ceil((size / 2.0)) << endl;
-	// for (int i = size - ceil((size / 2.0)); i < size; ++i)
-	// {
-	// 	cout << i << endl;
-	// }
-
 	int indexStartRand = size - ceil((size / 2.0));
 	int indexEndRand = size - 1;
 
 	//Indexes of chromosomes to be overrided by mating result.
-	// cout << "Offspring placeholder indexes: " << x << endl;
 	for (int i = numMutants; i < (size - ceil((size / 2.0))); ++i)
 	{
-		// cout << i << endl;
 		int parentAIndex = rand() % ((size) - indexStartRand) + indexStartRand;
 		int parentBIndex = rand() % ((size) - indexStartRand) + indexStartRand;
 
@@ -299,6 +216,7 @@ void Population::mateIndividuals(void) {
 		}
 
 		population[i].setResetGenes(matePair(population[parentAIndex].getChromosomeAsArray(), population[parentBIndex].getChromosomeAsArray()));
-		//Chromosome 'i' must be replaced by a mating result.
 	}
 }
+
+/* Private methods */
--- standard_rkga/population_class.h
+++ standard_rkga/population_class.h
@@ -8,29 +8,31 @@ using namespace std;
 class Population {
   public:
     Population();
-    // Chromosome * getElite(void); /* Returns an array containing the top two vectors. */
-    // Chromosome * getRandomPair(void); /* Returns an array containing two random vectors. */
+
+    /* Getters */
     Hallele * matePair(Hallele *a, Hallele *b); /* Returns the offspring of a pair of chromosomes. */
+    Hallele * getSolutionAsArray(int solIndex); /* Returns the chromosome sequence of alleles - RKGA encoding */
+    Chromosome getSingleChromosome(int index); /* Returns the object instance of one of the chromosomes within the population. */
+
+    /* Setters */
+    void initializePopulation(int popSize, int chromSize); /* Sets population size and solution array length. */
+    void updateFitness(int index, double fitness); /* Updates the fitness of a solution indexed by 'index'. */
+    void mateIndividuals(void); /* Produces offsprings of solutions - 'popSize' even 25% of population | 'popSize' odd 20% of population */
+    void introduceMutants(); /* Replaces 25% of the 'non-elit' halfh with mutant individuals. */
+    void sortByFitness(void); /* Sorts the population in descending ascending order based on the fitness values. */
+
+    /* Utils */
     void printPopulation(void); /* Prints each vector of the current population. */
     void printFitness(void); /* Prints each vector's fitness value of the current population. */
-    void initializePopulation(int popSize, int chromSize);
-    void updateFitness(int index, double fitness); /* Updates the fitness of a solution indexed by 'index'. */
 
-    void mateIndividuals(void);
-    void introduceMutants(); /* Replaces 25% of the 'non-elit' halfh with mutant individuals. */
-    Hallele * getSolutionAsArray(int solIndex);
-    // bool initializeUpdateDemand(int ind, double demand);
-    Chromosome getSingleChromosome(int index);
-
-    void swap(Chromosome *a, Chromosome *b);
-    int partition(Chromosome *array, int low, int high);
-    void quickSort(Chromosome *array, int low, int high);
-    void sortByFitness(void);
-    // void evolve(); /* Some of the above functions should be private. The evolve method will use 'em to manipulate the vectors. */
+    /* Utilities */
+    void swap(Chromosome *a, Chromosome *b); /* Utilized to swap two soltuions within the solution array. */
+    int partition(Chromosome *array, int low, int high); /* Utilized within the method performing quicksort. */
+    void quickSort(Chromosome *array, int low, int high); /* Quicksort utilized to sort population by fitness. */
 
   private:
-    int size;
-    Chromosome *population;
+    int size; /* Number of individuals within the population. */
+    Chromosome *population; /* Population as array of chromosomes. */
 };
 
 #endif
